43f7056614dd95e5c1b88f451dd4ad5d6c67742c
rachels-archive <rachelhui@protonmail.com>
2024-10-21 17:26:29 +0800

    Implement GET endpoint for contacts

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require("express");
+import express from "express";
+import contactRouter from "./routes/contactRouter.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+// Middleware
+app.use("/api/contacts", contactRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
