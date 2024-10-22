b72b98a3beba6595af62ce5d05af2c6e629a669f
rachels-archive <rachelhui@protonmail.com>
2024-10-22 16:24:53 +0800

    Throw error for better error handling

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -15,7 +15,8 @@ export const getAllContacts = asyncHandler(async (req, res) => {
 export const getContactById = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   if (!contact) {
-    res.status(404).json({ message: "Contact Not Found!" });
+    res.status(404);
+    throw new Error("Contact Not Found!");
   }
   res.status(200).json(contact);
 });
@@ -27,7 +28,8 @@ export const createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
 
   if (!name || !email || !phone) {
-    res.status(400).json({ message: "All fields are required!" });
+    res.status(400);
+    throw new Error("All fields are required!");
   }
 
   const contact = await Contact.create({
@@ -46,7 +48,8 @@ export const updateContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
 
   if (!contact) {
-    res.status(404).json({ message: "Contact Not Found!" });
+    res.status(404);
+    throw new Error("Contact Not Found!");
   }
 
   const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
@@ -59,7 +62,8 @@ export const updateContact = asyncHandler(async (req, res) => {
 export const deleteContact = asyncHandler(async (req, res) => {
   const deletedContact = await Contact.findByIdAndDelete(req.params.id);
   if (!deletedContact) {
-    return res.status(404).json({ message: "Contact not found." });
+    res.status(404);
+    throw new Error("Contact Not Found!");
   }
   res.status(204).send("Contact Successfully Deleted!");
 });
